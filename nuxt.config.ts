cdbe0389f99c37021d755d7d8cfa9bf17cb0670d
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-24 22:38:18 +0200

    feat: install service worker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
+import process from 'node:process';
+
+const sw = process.env.SW === 'true';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -15,6 +19,53 @@ export default defineNuxtConfig({
   ],
 
   ssr: false,
+
+  pwa: {
+    strategies: sw ? 'injectManifest' : 'generateSW',
+    srcDir: sw ? 'service-worker' : undefined,
+    filename: sw ? 'sw.ts' : undefined,
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'De Burgerlijke App',
+      short_name: 'DeBurgerlijkeApp',
+      theme_color: '#40939a',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
 
   runtimeConfig: {
     openaiApiKey: process.env.OPENAI_API_KEY,
--- plugins/pwa.client.ts
+++ plugins/pwa.client.ts
@@ -1,22 +1,27 @@
+import { consola } from 'consola';
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook(
     'service-worker:registered',
     ({ url, registration }) => {
       // eslint-disable-next-line no-console
-      console.log(
+      consola.info(
         `service worker registered at ${url}`,
         registration
       );
     }
   );
   nuxtApp.hook('service-worker:registration-failed', ({ error }) => {
-    console.error(`service worker registration failed`, error);
+    consola.error(`service worker registration failed`, error);
   });
   nuxtApp.hook(
     'service-worker:activated',
     ({ url, registration }) => {
       // eslint-disable-next-line no-console
-      console.log(`service worker activated at ${url}`, registration);
+      consola.success(
+        `service worker activated at ${url}`,
+        registration
+      );
     }
   );
 });
