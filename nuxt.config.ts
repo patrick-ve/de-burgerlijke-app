41ae1163871e9fb2f594dc47afdd52110a9c026e
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-25 12:57:07 +0200

    fix: increase cached file size

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,6 +50,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
     },
     injectManifest: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
