2b66c067b452b0a23386f9c804af247f6863e720
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-29 20:42:04 +0200

    wip: update layout

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,36 @@
 <template>
   <header
-    class="sticky top-0 h-16 z-40 bg-white border-b border-gray-200 p-4 grid grid-cols-[1fr_auto_1fr] items-center gap-4"
+    class="sticky top-0 h-16 z-40 bg-white border-b border-gray-200 p-4 items-center gap-4 w-full"
   >
-    <div>
-      <!-- Target for the left action teleport -->
-      <div id="header-left-action"></div>
-      <slot name="left-action" />
-    </div>
-    <h1 class="text-xl font-semibold truncate my-auto">
-      {{ title }}
-    </h1>
-    <div class="flex justify-end">
-      <!-- Target for the right action teleport -->
-      <div id="header-right-action"></div>
-      <slot name="right-action" />
+    <div
+      class="max-w-7xl mx-auto"
+      :class="[
+        !isOnHomePage
+          ? 'grid grid-cols-[1fr_auto_1fr]'
+          : 'flex justify-between',
+      ]"
+    >
+      <div v-if="!isOnHomePage">
+        <!-- Target for the left action teleport -->
+        <div id="header-left-action"></div>
+        <slot name="left-action" />
+      </div>
+      <h1
+        v-if="!isOnHomePage"
+        class="text-xl font-semibold truncate my-auto"
+      >
+        {{ title }}
+      </h1>
+      <div v-if="isOnHomePage">
+        <h1 class="text-xl font-semibold my-auto text-primary-500">
+          De Burgerlijke App
+        </h1>
+      </div>
+      <div class="flex justify-end">
+        <!-- Target for the right action teleport -->
+        <div id="header-right-action"></div>
+        <slot name="right-action" />
+      </div>
     </div>
   </header>
 </template>
@@ -24,6 +41,9 @@ interface Props {
 }
 
 defineProps<Props>();
+
+const route = useRoute();
+const isOnHomePage = computed(() => route.path === '/');
 </script>
 
 <style scoped>
@@ -32,5 +52,9 @@ defineProps<Props>();
 #header-left-action:empty,
 #header-right-action:empty {
   display: none;
+}
+
+.font-caveat {
+  font-family: 'Caveat', cursive;
 }
 </style>
